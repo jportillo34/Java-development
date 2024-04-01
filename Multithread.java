751dfe0c12bc45908002df30c0b0e0a3371e39e0
Jose Portillo <jportillo34@gmail.com>
2024-04-01 14:42:29 +0200

    Nuevo cambio

--- Multithread.java
+++ Multithread.java
@@ -18,6 +18,8 @@ class Multithread {
 	public static void main(String[] args) {
 		int n = 8; // Number of threads
 
+		System.out.println("Inicio del programa");
+
 		for (int i = 0; i < n; i++) {
 			Thread object = new Thread(new MultithreadingDemo());
 			object.start();
